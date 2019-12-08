303c375f6b279dff8d1d1bbfffba947606895a36
Tony Vo <ttrung.vo@gmail.com>
2019-12-08 04:09:16 -0700

    reorganize kubernetes files

--- kubectl-apply.sh
+++ kubectl-apply.sh
@@ -10,15 +10,15 @@ logSummary(){
     echo "#####################################################"
 }
 
-kubectl apply -f auctionsniper/
-kubectl apply -f messagebroker/
-kubectl apply -f monitoring/jhipster-prometheus-crd.yml
+kubectl apply -f kubernetes/auctionsniper/
+kubectl apply -f kubernetes/messagebroker/
+kubectl apply -f kubernetes/monitoring/jhipster-prometheus-crd.yml
 until [ $(kubectl get crd prometheuses.monitoring.coreos.com 2>>/dev/null | wc -l) -ge 2 ]; do
     echo "Waiting for the custom resource prometheus operator to get initialised";
     sleep 5;
 done
-kubectl apply -f monitoring/jhipster-prometheus-cr.yml
-kubectl apply -f monitoring/jhipster-grafana.yml
-kubectl apply -f monitoring/jhipster-grafana-dashboard.yml
+kubectl apply -f kubernetes/monitoring/jhipster-prometheus-cr.yml
+kubectl apply -f kubernetes/monitoring/jhipster-grafana.yml
+kubectl apply -f kubernetes/monitoring/jhipster-grafana-dashboard.yml
 
 logSummary
