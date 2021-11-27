7f7402d5440290d23ae0fae09ffef326b8db5f6a
Tony Vo <ttrung.vo@gmail.com>
2021-11-27 06:15:13 -0700

    mob next [ci-skip] [ci skip] [skip ci]

---
+++ src/test/java/org/auctionsniper/AuctionSniperEndToEndTest.java
@@ -0,0 +1,17 @@
+package org.auctionsniper;
+
+import org.junit.jupiter.api.Test;
+
+public class AuctionSniperEndToEndTest {
+    private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
+    private final ApplicationRunner application = new ApplicationRunner();
+
+    @Test
+    public void sniperJoinsAuctionUntilAuctionCloses() throws Exception {
+        auction.startSellingItem();
+        application.startBiddingIn(auction);
+        auction.hasReceivedJoinRequestFromSniper();
+        auction.announceClosed();
+        application.showsSniperHasLostAuction();
+    }
+}
